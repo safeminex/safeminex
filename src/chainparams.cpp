dfb6e608f185cabcff50a83c9c4b528b6171fcaa
Alfariqi <92787297+alfariqicrypto@users.noreply.github.com>
2022-09-10 10:27:47 +0700

    dnsseed.safeminecoin.xyz

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,10 +124,9 @@ public:
         assert(consensus.hashGenesisBlock == uint256S("0xa5de2882f46d3f67da122eede2cba76ebb0b39fdef96fe6e1e45efd3c47e53ed"));
         assert(genesis.hashMerkleRoot == uint256S("0xdf99203df0f798091f888094498ed75198b0f4e8a8af4aad3979059a55201f06"));
 
+        vSeeds.push_back(CDNSSeedData("dnsseed.safeminecoin.xyz", "dnsseed.safeminecoin.xyz"));
         vSeeds.push_back(CDNSSeedData("192.99.252.243", "192.99.252.243"));
-        vSeeds.push_back(CDNSSeedData("135.125.225.55", "135.125.225.55"));
-        vSeeds.push_back(CDNSSeedData("51.195.249.132", "51.195.249.132"));
-        vSeeds.push_back(CDNSSeedData("addnode=dnsseed.safeminecoin.xyz", "addnode=dnsseed.safeminecoin.xyz"));
+        vSeeds.push_back(CDNSSeedData("51.195.249.132", "51.77.48.45"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,75);
